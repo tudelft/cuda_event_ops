4563179bf3182a410607c2bf6769fca9fa126ce4
Huizerd <15855769+Huizerd@users.noreply.github.com>
2024-10-11 15:55:47 +0200

    build for cuda when no gpu yet

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 from setuptools import find_packages, setup
+import os
+
+import torch
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+
+# set cuda arch if cross-compiling and gpu not available (e.g. during docker build)
+# https://en.wikipedia.org/wiki/CUDA#GPUs_supported
+if not torch.cuda.is_available():
+    if os.environ.get("TORCH_CUDA_ARCH_LIST") is None:
+        os.environ["TORCH_CUDA_ARCH_LIST"] = "7.5;8.0;8.6;8.7;8.9+PTX"  # turing, ampere, ada
 
 
 setup(
