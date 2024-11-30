0ea7cb29b5d6f0fab2315b5cf0145007b6cd154a
Huizerd <15855769+Huizerd@users.noreply.github.com>
2024-11-30 13:15:48 +0100

    docs

--- cuda_3d_ops/torch_batch.py
+++ cuda_3d_ops/torch_batch.py
@@ -4,58 +4,62 @@ import torch.nn.functional as F
 
 def linear_warp(events, flow, t_ref, keep_ts=True):
     """
-    Warp events linearly to reference time.
+    Warps events linearly to reference time.
     Changes events in-place, i.e. does not copy data.
 
     Args:
-        events: (*, b, 4, n) tensor of events with (ts, y, x, p) in -2 dim.
-        flow: (*, b, 2, n) tensor of (y, x) flow for each event.
-        t_ref: (*, 1, 1, 1) tensor or scalar of reference time to warp to.
+        events (torch.Tensor): (b, d, 4, n) tensor of events with (x, y, z, val) in -2 dim.
+        flow (torch.Tensor): (b, d, 2, n) tensor of (u, v) flow for each event.
+        t_ref (torch.Tensor): (b, d, 1, 1) tensor or scalar of reference time to warp to.
 
     Returns:
-        (*, b, 4, n) tensor of warped events.
+        torch.Tensor: (b, d, 4, n) tensor of warped events.
     """
-    events[..., 1:3, :] += (t_ref - events[..., 0:1, :]) * flow
+    events[..., 0:2, :] += (t_ref - events[..., 2:3, :]) * flow
     if not keep_ts:
-        events[..., 0:1, :] = t_ref
+        events[..., 2:3, :] = t_ref
     return events
 
 
 def get_event_flow_3d(events, flow):
     """
+    Bilinearly samples flow at event locations.
+
     Args:
-        events: (d, b, 4, n) tensor of events with (ts, y, x, p) in -2 dim.
-        flow: (d, b, 2, h, w) tensor of (y, x) flow maps.
+        events (torch.Tensor): A tensor of shape (b, d, 4, n), where each event has (x, y, z, val).
+        flow (torch.Tensor): A tensor of shape (b, d, h, w, 2), where each flow has (u, v).
 
     Returns:
-        (d, b, 2, n) tensor of (y, x) flow for each event.
+        torch.Tensor: A tensor of shape (b, d, 2, n) of (u, v) flow for each event.
     """
 
     # discretize ts (no bilinear in time)
     # grid: order (x, y, t) in last dim and normalize to (-1, 1)
-    d, _, _, h, w = flow.shape
-    y, x = events[..., 1:3, :].unbind(-2)
-    z = torch.ones_like(x) * torch.arange(d, dtype=events.dtype, device=events.device).view(-1, 1, 1)
+    _, d, h, w, _ = flow.shape
+    x, y = events[..., 0:2, :].unbind(-2)
+    z = torch.ones_like(x) * torch.arange(d, dtype=events.dtype, device=events.device).view(-1, 1)
     resolution = torch.tensor([w, h, d], dtype=events.dtype, device=events.device)  # needs clamp if d == 1
     grid_norm = 2 * torch.stack([x, y, z], dim=-1).unsqueeze(3) / (resolution - 1).clamp(min=1) - 1
 
     # use grid_sample so idx can be float
     # align corners because warping to pixel centers
-    # flow shape: (b, 2, d, n, w); idx_norm shape: (b, d, n, 1, 3)
+    # flow shape: (b, d, h, w, 2) -> (b, 2, d, h, w); idx_norm shape: (b, d, n, 1, 3)
     # returns (b, 2, d, n, 1)
-    flow = F.grid_sample(flow.permute(1, 2, 0, 3, 4), grid_norm.transpose(0, 1), mode="bilinear", align_corners=True)
+    flow = F.grid_sample(flow.permute(0, 4, 1, 2, 3), grid_norm, mode="bilinear", align_corners=True)
 
-    return flow.squeeze(-1).permute(2, 0, 1, 3)
+    return flow.squeeze(-1).permute(0, 2, 1, 3)
 
 
 def compute_inside_mask(idx, resolution):
     """
+    Computes a mask of events inside resolution.
+
     Args:
-        idx: (*, b, 2, n) tensor of (y, x) event indices in -2 dim.
-        resolution: (2,) tensor of (h, w) resolution.
+        idx (torch.Tensor): (b, d, 2, n) tensor of (x, y) event indices in -2 dim.
+        resolution (torch.Tensor): (2,) tensor of (w, h) resolution.
 
     Returns:
-        (*, b, 1, n) mask of events inside resolution.
+        torch.Tensor: (b, d, 1, n) mask of events inside resolution.
     """
     # in taming: idx >= 0 & idx <= resolution - 1
     # ensures enough space for a whole pixel
@@ -64,28 +68,27 @@ def compute_inside_mask(idx, resolution):
     return mask.all(dim=-2, keepdim=True)
 
 
-def iterative_3d_warp(events, flows, base):
+def iterative_3d_warp(events, flows, num_warps):
     """
+    Iteratively warps events in 3D using bilinearly-sampled flows.
+
     Args:
-        events: (d, b, 4, n) tensor of events with (ts, y, x, p) in -2 dim.
-        flows: (d, b, 2, h, w) tensor of (y, x) flow maps.
-        base: int of number of neighboring bins (one-sided) to consider for each reference time.
+        events (torch.Tensor): A tensor of shape (b, n, 5), where each event has (x, y, z, zi, val).
+        flows (torch.Tensor): A tensor of shape (b, d, h, w, 2), where each flow has (u, v).
+        num_warps (int): The number of warping stages.
 
     Returns:
-        torch.Tensor: A tensor of shape (d + 1, b, 4, n) with the warped events.
+        torch.Tensor: A (d + 1, d, b, 4, n) tensor of warped events where each event has (x, y, z, val).
     """
-    # NOTE: temporary
-    b, d, _, _, _ = flows.shape
-    events = events.view(b, d, -1, 5).contiguous()
-    events = events[..., [2, 1, 0, 4]].permute(1, 0, 3, 2).contiguous()
-    flows = flows[..., [1, 0]].permute(1, 0, 4, 2, 3).contiguous()
 
     # get deblurring window and resolution
-    d, _, _, h, w = flows.shape
-    resolution = torch.tensor([h, w], device=flows.device)
+    b, d, h, w, _ = flows.shape
+    resolution = torch.tensor([w, h], device=flows.device)
 
     # repeat for simultaneous fw and bw warping
-    events = events.repeat(2, 1, 1, 1)  # copies
+    # (b, n, 5) -> (b, d, 4, n)
+    events = events[..., [0, 1, 2, 4]].view(b, d, -1, 4).transpose(2, 3)
+    events = events.repeat(1, 2, 1, 1)  # copies
 
     # lists to store warped events, ts and inside mask
     # we need to know where they came from + where they're going
@@ -104,8 +107,8 @@ def iterative_3d_warp(events, flows, base):
 
         # select events, flow and original timestamps (for scaling)
         # advanced indexing copies
-        select_events = events[t_events]
-        select_flow = flows[t_flow]
+        select_events = events[:, t_events]
+        select_flow = flows[:, t_flow]
 
         # sample flow that will warp events at event locations
         # ensure integer t because we don't want bilinear there
@@ -115,23 +118,23 @@ def iterative_3d_warp(events, flows, base):
         # fw/bw warp events to reference time
         # overwrite ts with reference time
         # all in-place
-        select_events = linear_warp(select_events, select_event_flow, t_ref.view(-1, 1, 1, 1), keep_ts=False)
+        select_events = linear_warp(select_events, select_event_flow, t_ref.view(1, -1, 1, 1), keep_ts=False)
 
         # discard events warped outside image
-        mask_inside = compute_inside_mask(select_events[..., 1:3, :], resolution)
-        select_events[..., 1:4, :] *= mask_inside  # mask idx and p, can be done in-place
+        mask_inside = compute_inside_mask(select_events[..., 0:2, :], resolution)
+        select_events[..., 3:4, :] *= mask_inside  # mask p, can be done in-place
 
         # save warped events, no copy here
-        events[t_events] = select_events
+        events[:, t_events] = select_events
 
         # save warped events, ts and inside mask for border compensation
         # also take into account mask outside base
         for j, (src, dst) in enumerate(zip([*t0, *t1], t_ref)):
-            warped_events[dst][src] = select_events[j : j + 1] if i < base else None
+            warped_events[dst][src] = select_events[:, j : j + 1] if i < num_warps else None
             warped_mask_inside[src] = (
-                warped_mask_inside[src] * mask_inside[j : j + 1]
+                warped_mask_inside[src] * mask_inside[:, j : j + 1]
                 if warped_mask_inside[src] is not None
-                else mask_inside[j : j + 1]
+                else mask_inside[:, j : j + 1]
             )
 
     # concat at each reference time
@@ -145,28 +148,30 @@ def iterative_3d_warp(events, flows, base):
 
 def inv_l1_dist_prod(idx):
     """
+    Computes the inverse product of L1 distances to closest pixels.
+
     Args:
-        idx: (*, b, 2, n) tensor of (y, x) event indices in -2 dim.
+        idx (torch.Tensor): (d, b, 2, n) tensor of (x, y) event indices in -2 dim.
 
     Returns:
-        (*, b, 2, 4n) tensor of nearest integer indices.
-        (*, b, 1, 4n) tensor of inverse L1 distances.
+        torch.Tensor: (d, b, 2, 4n) tensor of nearest integer indices.
+        torch.Tensor: (d, b, 1, 4n) tensor of inverse L1 distances.
     """
 
     # 1d rounded sides
     # doing idx.floor() + 1 is different from doing (idx.floor() + 1)
     # due to floating point precision; event_flow does latter so keep that
     # ceil is different from floor + 1 for integers
-    top_y = idx[..., 0, :].floor()
-    bot_y = (idx[..., 0, :] + 1).floor()
-    left_x = idx[..., 1, :].floor()
-    right_x = (idx[..., 1, :] + 1).floor()
+    left_x = idx[..., 0, :].floor()
+    right_x = (idx[..., 0, :] + 1).floor()
+    top_y = idx[..., 1, :].floor()
+    bot_y = (idx[..., 1, :] + 1).floor()
 
     # combine to 2d corners
-    top_left = torch.stack([top_y, left_x], dim=-2)
-    top_right = torch.stack([top_y, right_x], dim=-2)
-    bottom_left = torch.stack([bot_y, left_x], dim=-2)
-    bottom_right = torch.stack([bot_y, right_x], dim=-2)
+    top_left = torch.stack([left_x, top_y], dim=-2)
+    top_right = torch.stack([right_x, top_y], dim=-2)
+    bottom_left = torch.stack([left_x, bot_y], dim=-2)
+    bottom_right = torch.stack([right_x, bot_y], dim=-2)
 
     # corners sequentially in event dimension
     idx_round = torch.cat([top_left, top_right, bottom_left, bottom_right], dim=-1)
@@ -185,44 +190,53 @@ def inv_l1_dist_prod(idx):
 
 def accumulate_to_image(idx, weight, mask, resolution):
     """
+    Accumulates weighted events to image.
+
     Args:
-        idx: (*, b, 2, n) tensor of (y, x) event indices in -2 dim.
-        weight: (*, b, 1, n) tensor of weights to accumulate.
-        mask: (*, b, 1, n) mask tensor.
-        resolution: (2,) tensor of (h, w) resolution.
+        idx (torch.Tensor): (d, b, 2, n) tensor of (x, y) event indices in -2 dim.
+        weight (torch.Tensor): (d, b, 1, n) tensor of weights to accumulate.
+        mask (torch.Tensor): (d, b, 1, n) mask tensor.
+        resolution (torch.Tensor): (2,) tensor of (h, w) resolution.
 
     Returns:
-        (*, b, 1, h, w) tensor of accumulated images.
+        (d, b, 1, h, w) tensor of accumulated images.
     """
 
     # 1d image to accumulate to
     image = torch.zeros(*idx.shape[:-2], 1, resolution.prod(), dtype=idx.dtype, device=idx.device)
 
     # convert idx to 1d
-    idx = (idx[..., 0:1, :] * resolution[1] + idx[..., 1:2, :]).long()
+    idx = (idx[..., 1:2, :] * resolution[1] + idx[..., 0:1, :]).long()
 
     # accumulate and reshape to 2d
     image.scatter_add_(-1, idx, weight * mask)
-    # image = scatter_add(weight * mask, idx, dim=-1)
     return image.unflatten(-1, resolution.tolist())
 
 
 def trilinear_splat(events, grid_resolution):
+    """
+    Accumulates events to 3D grid using trilinear interpolation.
+    
+    Args:
+        events (torch.Tensor): (d + 1, d, b, 4, n) tensor of warped events with (x, y, z, val) in -2 dim.
+        grid_resolution (tuple): (d, h, w) resolution of the grid.
+    
+    Returns:
+        torch.Tensor: (b, d, h, w) tensor of accumulated images.
+    """
+
     # get deblurring window and resolution
     _, _, b, _, _ = events.shape
     d, h, w = grid_resolution
     resolution = torch.tensor([h, w], device=events.device)
 
-    # NOTE: temporary
-    d -= 1
-
     # iwe and iwt at each reference time
-    iwe = torch.zeros(d + 1, b, 1, h, w, dtype=events.dtype, device=events.device)
+    iwe = torch.zeros(d, b, 1, h, w, dtype=events.dtype, device=events.device)
 
     # build iwe and iwt
-    for i in range(d + 1):
+    for i in range(d):
         # inverse product of l1 distances to closest pixels
-        corners, weights = inv_l1_dist_prod(events[i][..., 1:3, :])
+        corners, weights = inv_l1_dist_prod(events[i][..., 0:2, :])
 
         # discard corners outside image
         mask_inside = compute_inside_mask(corners, resolution)
@@ -233,7 +247,4 @@ def trilinear_splat(events, grid_resolution):
         values = events[i][..., 3:4, :].repeat(1, 1, 1, 4)  # repeat for corners
         iwe[i] += accumulate_to_image(corners, weights * values, torch.ones_like(weights), resolution).sum(0)
 
-    # NOTE: temporary
-    iwe = iwe.squeeze(2).permute(1, 0, 2, 3).contiguous()
-
-    return iwe
+    return iwe.squeeze(2).permute(1, 0, 2, 3)
